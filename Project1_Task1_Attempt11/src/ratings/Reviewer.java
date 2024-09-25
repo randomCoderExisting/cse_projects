e176911711006f03dbb523ee4c73aed5ccc62264
Effective_Editor913 <112575656+randomCoderExisting@users.noreply.github.com>
2024-09-25 22:05:32 +0800

    Java Task 1 Test Upload

---
+++ Project1_Task1_Attempt11/src/ratings/Reviewer.java
@@ -0,0 +1,36 @@
+package ratings;
+
+public class Reviewer {
+
+    /*
+    This class will have a constructor that takes 1 parameter:
+    1. A String representing the ID of the reviewer
+    */
+    private String ID;
+
+    public Reviewer(String ID) {
+        this.ID = ID;
+    }
+
+    /*
+    Implement getter and setter methods for the constructor parameter named:
+    1. getReviewerID
+    2. setReviewerID
+    */
+    public String getReviewerID() {
+        return this.ID;
+    }
+
+    public void setReviewerID(String ID) {
+        this.ID = ID;
+    }
+
+    /*
+    Implement a method named rateSong that takes an int as a parameter and returns
+    a new Rating object with this reviewer's ID and the rating from the parameter.
+    */
+    public Rating rateSong(int input) {
+        return new Rating(this.ID, input);
+    }
+
+}
